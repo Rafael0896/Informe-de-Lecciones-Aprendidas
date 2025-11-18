1b70f72b7be1f029cef8935acfb308f4717b1d8c
Rafael0896 <realvarez54@misena.edu.co>
2025-11-18 18:57:40 -0500

    Update index.html; add App.jsx; rename main.js to main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="app" role="application" aria-label="Informe de Lecciones Aprendidas">
+      <div>
+        <main role="main">
+        </main>
+      </div>
+    </div>
+  );
+}
